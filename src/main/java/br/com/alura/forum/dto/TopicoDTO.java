aa188d355a877f34731dd7fae058c1d2b274106c
Bruno Santos <brunosansp@outlook.com>
2020-05-26 08:43:45 -0300

    first commit with TopicosController and DTO

---
+++ src/main/java/br/com/alura/forum/dto/TopicoDTO.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.model.Topico;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TopicoDTO {
+
+	private Long id;
+	
+	private String titulo;
+	
+	private String mensagem;
+	
+	private LocalDateTime dataCriacao;
+
+	public TopicoDTO(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+	}
+
+	// Converção de Topico para TopicoDTO
+	public static List<TopicoDTO> converter(List<Topico> topicos) {
+		return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+	}
+}
